33d11bc39fdc355ca57fb6601cd94b384169adc1
Tom <tombryden@me.com>
2024-03-12 00:04:55 +0000

    Added textarea + rhf + form submission mutation

--- src/components/pages/home/main-table/add-question-dialog.tsx
+++ src/components/pages/home/main-table/add-question-dialog.tsx
@@ -12,11 +12,68 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { graphql } from "@/gql";
+import { AllRecordsDocument } from "@/gql/graphql";
+import { useMutation } from "@apollo/client";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface AddQuestionForm {
+  question: string;
+  answer: string;
+  createdBy: string;
+  assignee: string;
+}
+
+const CREATE_RECORD_MUT = graphql(`
+  mutation createRecord2($createRecordInput: CreateRecordInput!) {
+    createRecord(createRecordInput: $createRecordInput) {
+      _recordId
+    }
+  }
+`);
 
 export default function AddQuestionDialog() {
+  const [open, setOpen] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<AddQuestionForm>();
+
+  const [createRecordMut, { loading }] = useMutation(CREATE_RECORD_MUT);
+
+  const onSubmit = handleSubmit((formData) => {
+    const { question, answer, createdBy, assignee } = formData;
+
+    createRecordMut({
+      variables: {
+        createRecordInput: {
+          question,
+          answer,
+          createdBy,
+          assignee,
+        },
+      },
+      awaitRefetchQueries: true,
+      refetchQueries: [AllRecordsDocument],
+      onCompleted: (data) => {
+        setOpen(false);
+
+        // reset fields 500 ms after closing so no flicker on fields
+        setTimeout(() => {
+          reset();
+        }, 500);
+      },
+    });
+  });
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button className="mb-10">
           <Plus className="mr-2 w-5 h-5" />
@@ -32,30 +89,54 @@ export default function AddQuestionDialog() {
           </DialogDescription>
         </DialogHeader>
 
-        <div className="my-4 space-y-4">
-          <div>
-            <Label htmlFor="question">Question *</Label>
-            <Input id="question" placeholder="Enter a question" />
-          </div>
-          <div>
-            <Label htmlFor="answer">Answer</Label>
-            <Input id="answer" placeholder="Enter an answer" />
+        <form onSubmit={onSubmit}>
+          <div className="my-4 space-y-4">
+            <div>
+              <Label htmlFor="question">Question *</Label>
+              <Textarea
+                id="question"
+                placeholder="Enter a question"
+                {...register("question", { required: "Field required" })}
+                error={errors.question?.message}
+              />
+            </div>
+            <div>
+              <Label htmlFor="answer">Answer</Label>
+              <Textarea
+                id="answer"
+                placeholder="Enter an answer"
+                {...register("answer")}
+                error={errors.answer?.message}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="createdBy">Created By *</Label>
+              <Input
+                id="createdBy"
+                placeholder="Created by email"
+                {...register("createdBy", { required: "Field required" })}
+                error={errors.createdBy?.message}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="assignee">Assignee</Label>
+              <Input
+                id="assignee"
+                placeholder="Assignee email"
+                {...register("assignee")}
+                error={errors.assignee?.message}
+              />
+            </div>
           </div>
 
-          <div>
-            <Label htmlFor="createdBy">Created By *</Label>
-            <Input id="createdBy" placeholder="Created by email" />
-          </div>
-
-          <div>
-            <Label htmlFor="assignee">Assignee *</Label>
-            <Input id="assignee" placeholder="Assignee email" />
-          </div>
-        </div>
-
-        <DialogFooter>
-          <Button type="submit">Save Changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit" loading={loading}>
+              Save Changes
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,6 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  mutation createRecord2($createRecordInput: CreateRecordInput!) {\n    createRecord(createRecordInput: $createRecordInput) {\n      _recordId\n    }\n  }\n": types.CreateRecord2Document,
     "\n  query AllRecords {\n    records {\n      _recordId\n      question\n      answer\n      createdBy\n      createdAt\n      assignedTo\n    }\n  }\n": types.AllRecordsDocument,
 };
 
@@ -30,6 +31,10 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation createRecord2($createRecordInput: CreateRecordInput!) {\n    createRecord(createRecordInput: $createRecordInput) {\n      _recordId\n    }\n  }\n"): (typeof documents)["\n  mutation createRecord2($createRecordInput: CreateRecordInput!) {\n    createRecord(createRecordInput: $createRecordInput) {\n      _recordId\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
